b8e380d304c4c20cc71fd372a8fc4e0568bf41f4
Register <CGR537@multihosp.net>
2025-12-16 14:05:17 -0500

    Rough improvements to the formatting of the markdown toggle.

--- lua/plugins/render_markdown.lua
+++ lua/plugins/render_markdown.lua
@@ -8,7 +8,22 @@ return {
     },
     opts = {},
     keys = {
-      { "<leader>tm", function() require("render-markdown").toggle() end, ft = "markdown", desc = "[t]oggle [m]arkdown rendering." },
+      { "<leader>tm", function()
+        require("render-markdown").toggle()
+        -- Lightweight "zen-mode"
+        if vim.bo.filetype == "markdown" then
+          vim.wo.number = false
+          vim.wo.relativenumber = false
+          vim.wo.signcolumn = "no"
+          vim.wo.cursorline = false
+          vim.bo.textwidth = 80          -- used by gq / formatting
+          vim.wo.conceallevel = 2        -- hide markup noise a bit
+          vim.wo.concealcursor = "nc"    -- keep it readable while moving
+          vim.wo.wrap = true
+          vim.wo.linebreak = true        -- wrap on words
+          vim.wo.breakindent = true
+        end
+      end, ft = "markdown", desc = "[t]oggle [m]arkdown rendering." },
     },
   },
 }
